a867eddbb37034d4b699b9f0f59d8e341dab8153
程玉辉 <yuhui33551234@163.com>
2019-06-28 23:00:59 +0800

    ii

--- pu1.js
+++ pu1.js
@@ -1,4 +1,11 @@
-//
+'use strict'
 
-//调度
-//ddd
+const puppeteer = require('puppeteer');
+
+(async()=>{
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://example.com');
+    await page.screenshot({path: 'example.png'});
+    await browser.close();
+})();
